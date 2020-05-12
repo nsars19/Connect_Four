79df537c785b66f6f763e2fb1184808fa383a0fc
Nick <nsardonini@gmail.com>
2020-05-12 15:45:25 -0400

    add tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,25 @@
 require './lib/board'
+require_relative 'cell_spec'
 
 describe 'Board' do
-  context 'when initialized' do
-    it 'creates 42 cells'
+  describe '#place_piece' do
+    it 'places a piece'
+    it 'creates a cell'
+  end
+
+  describe '#over?' do
+    it 'returns true if win'
+    it 'returns false if no win'
+  end
+
+  describe '#check_if_win' do
+    it 'accepts a block'
+    it 'checks diagonally'
+    it 'checks vertically'
+    it 'checks horizontally'
+  end
+
+  describe '#display_board' do
+    it 'outputs to stdout'
   end
 end
