3ddc1578adbd8b348cf59b3bfc2b42f34e6f02f2
Nick <nsardonini@gmail.com>
2020-05-12 17:18:32 -0400

    create Cell class

---
+++ lib/cell.rb
@@ -0,0 +1,14 @@
+require_relative 'traversal'
+
+class Cell
+  include Traversable
+  attr_accessor :up_right, :up_left, :down_right, :down_left
+
+  def initialize color
+    @color = color
+    @up_right   = nil
+    @down_right = nil
+    @up_left    = nil
+    @down_left  = nil
+  end
+end
