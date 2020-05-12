7c6757efa2345f120ea387f3a53846ad7b470e8c
Nick <nsardonini@gmail.com>
2020-05-12 18:48:50 -0400

    change variable names

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,14 +2,14 @@ require_relative 'traversal'
 
 class Cell
   include Traversable
-  attr_accessor :up_right, :up_left, :down_right, :down_left
+  attr_accessor :upper_right, :upper_left, :lower_right, :lower_left
   attr_reader :color
 
   def initialize color
     @color = color
-    @up_right   = nil
-    @down_right = nil
-    @up_left    = nil
-    @down_left  = nil
+    @upper_right = nil
+    @lower_right = nil
+    @upper_left  = nil
+    @lower_left  = nil
   end
 end
