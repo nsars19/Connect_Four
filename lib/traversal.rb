8b573fa972277cfc30ba809d1aef75f20e201e83
Nick <nsardonini@gmail.com>
2020-05-12 17:53:54 -0400

    add #get_left_upper_diagonals

--- lib/traversal.rb
+++ lib/traversal.rb
@@ -1,3 +1,11 @@
 module Traversable
-  
+  def get_left_upper_diagonals
+    colors = [self.color]
+    node = self
+    until node.up_left.nil?
+      node = node.up_left
+      colors << node.color
+    end
+    colors
+  end
 end
