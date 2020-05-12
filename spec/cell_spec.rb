e95527512aac3dadc044e1e041bb624394cda1ba
Nick <nsardonini@gmail.com>
2020-05-12 19:23:26 -0400

    refactor traversal tests
    used string interpolation to pass direction values instead of writing four
    nearly-identical tests by hand

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,17 +2,23 @@ require './lib/cell'
 
 describe "Cell" do
   let(:cell) { Cell.new 'red' }
+  let(:node_2) { instance_double("Cell") }
+  let(:node_3) { instance_double("Cell") }
+  let(:colors) { ['red', 'red', 'red'] }
 
-  describe "#get_left_upper_diagonals" do
-    it 'gets upper-left nodes colors' do
-      node_2 = instance_double("Cell")
-      node_3 = instance_double("Cell")
-      allow(node_2).to receive_messages(:color => 'red')
-      allow(node_3).to receive_messages(:color => 'red')
-      allow(node_2).to receive_messages(:up_left => node_3)
-      allow(node_3).to receive_messages(:up_left => nil)
-      cell.up_left = node_2
-      expect(cell.get_left_upper_diagonals).to eql ['red', 'red', 'red']
+  ["upper", "lower"].each do |up_down|
+    ["left", "right"].each do |left_right|
+      describe "#get_#{up_down}_#{left_right}_diagonals" do
+        it "gets #{up_down}-#{left_right} nodes colors" do
+          [node_2, node_3].each do |node|
+            allow(node).to receive_messages(:color => "red")
+          end
+          allow(node_2).to receive_messages("#{up_down}_#{left_right}" => node_3)
+          allow(node_3).to receive_messages("#{up_down}_#{left_right}" => nil)
+          cell.send("#{up_down}_#{left_right}=", node_2)
+          expect(cell.send("get_#{up_down}_#{left_right}_diagonals")).to eql colors
+        end
+      end
     end
   end
 end
