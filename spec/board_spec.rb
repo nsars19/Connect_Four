ad4840abb281259a7d49e56369792c7280355aad
Nick <nsardonini@gmail.com>
2020-05-12 16:04:32 -0400

    add constructor test
    add examples to verify that 6 arrays of size 7 are created when a Board
    is instantiated

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,6 +2,19 @@ require './lib/board'
 require_relative 'cell_spec'
 
 describe 'Board' do
+  let(:game) { Board.new }
+
+  context 'when initialized' do
+    it 'creates 6 nested arrays' do
+      expect(game.board.size).to eql 6
+    end
+
+    it 'has nested arrays with size 7' do
+      all_sizes_7 = game.board.map(&:size).all?(7)
+      expect(all_sizes_7).to be true
+    end
+  end
+
   describe '#place_piece' do
     it 'places a piece'
     it 'creates a cell'
