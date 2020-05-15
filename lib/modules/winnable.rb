1cfe1159f69e98ebee2f5fff336d7c250d15b450
Nick <nsardonini@gmail.com>
2020-05-15 17:28:30 -0400

    add #diagonal_win? logic

--- lib/modules/winnable.rb
+++ lib/modules/winnable.rb
@@ -1,5 +1,15 @@
 module Winnable
-  def diagonal_win? color_array
+  def diagonal_win? line_array
+    line_array.each do |line|
+      next if line.size < 4
+      3.times do |i|
+        ['red', 'black'].each do |item|
+          all_same = line[i..(i + 3)].all? { |color| color == item }
+          return true if all_same 
+        end
+      end
+    end
+    false
   end
 
   def horizontal_win?
