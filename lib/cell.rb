6f3ddafb53cc24336ac5e7a5f3e0564b792c1e1e
Nick <nsardonini@gmail.com>
2020-05-15 18:36:48 -0400

    add @@count to track number of cells created

--- lib/cell.rb
+++ lib/cell.rb
@@ -6,6 +6,7 @@ class Cell
   include Winnable
   attr_accessor :upper_right, :upper_left, :lower_right, :lower_left
   attr_reader :color
+  @@count = 0
 
   def initialize color
     @color = color
@@ -13,5 +14,6 @@ class Cell
     @lower_right = nil
     @upper_left  = nil
     @lower_left  = nil
+    @@count += 1
   end
 end
