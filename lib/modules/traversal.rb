c9badc6afdbba9865ce8d6a9fba81b6c04734b6b
Nick <nsardonini@gmail.com>
2020-05-14 20:44:53 -0400

    add #get_diagonals, refactor #combine_diagonals

--- lib/modules/traversal.rb
+++ lib/modules/traversal.rb
@@ -14,13 +14,19 @@ module Traversable
   end
 
   def get_diagonals
+    up_left   = self.get_upper_left_diagonals
+    up_right  = self.get_upper_right_diagonals
+    low_left  = self.get_lower_left_diagonals
+    low_right = self.get_lower_right_diagonals
+    first_diagonal  = combine_diagonals(up_left, low_right)
+    second_diagonal = combine_diagonals(up_right, low_left)
+    [first_diagonal, second_diagonal]
   end
 
   def combine_diagonals upper, lower
     # Reverse an array so they each 'point' the same direction
-    upper = upper.reverse
     # Remove initial piece from one array so it is not counted twice 
-    combined_diagonal = upper + lower[1..-1]
+    lower[1..-1].reverse + upper
   end
 
   def get_horizontals board_array
@@ -34,5 +40,14 @@ module Traversable
   end
 
   def get_verticals board_array
+    columns = []
+    7.times do |horz_idx|
+      colors = []
+      6.times do |vert_idx|
+        colors << board_array[vert_idx][horz_idx].color
+      end
+      columns << [colors.join(' ')]
+    end
+    columns
   end
 end
