ded8f123c009bbc5b2ab279fdeef7ca9e509b3ae
Nick <nsardonini@gmail.com>
2020-05-16 16:49:50 -0400

    add #place_piece, #choose_again, and Player class

--- lib/board.rb
+++ lib/board.rb
@@ -10,4 +10,29 @@ class Board
   def initialize
     @board = Array.new(6) { Array.new(7) }
   end
+
+  def place_piece player_color, choice
+    @board.each do |row|
+      return row[choice - 1] = Cell.new(player_color) if row[choice - 1].nil?
+    end
+    new_choice = self.choose_again
+    place_piece(player_color, new_choice)
+  end
+
+  def choose_again
+    puts "please choose another space. that column is full!"
+    choice = STDIN.gets.chomp.to_i
+    until choice > 0 && choice < 8
+      choice = STDIN.gets.chomp.to_i
+    end
+    choice
+  end
+
+  class Player
+    attr_reader :color
+
+    def initialize color
+      @color = color
+    end
+  end
 end
