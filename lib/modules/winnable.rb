83c5844fe4550ff8be1e1b5d3524a2d8c165fe8d
Nick <nsardonini@gmail.com>
2020-05-17 18:56:54 -0400

    refactor #diagonal_win?

--- lib/modules/winnable.rb
+++ lib/modules/winnable.rb
@@ -3,8 +3,11 @@ module Winnable
     diagonals.each do |line|
       next if line.size < 4
       3.times do |i|
-        all_same = line[i..(i + 3)].all? { |color| color == line[i] }
-        return true if all_same 
+        test_group = line[i..(i + 3)]
+        if test_group.is_a?(Array) && test_group.size == 4
+          all_same = test_group.all? { |item| item == test_group[0] }
+          return true if all_same
+        end
       end
     end
     false
@@ -16,7 +19,9 @@ module Winnable
       # iterate over array to check all spaces. 4 times is the minimum
       # amount of steps needed to check all positions
       board.each do |line_array|
+        next if line_array.all?(&:nil?)
         4.times do |i|
+          next if line_array.size < 4
           test_group = line_array[i..(i + 3)]
           if test_group.all? { |color| color == line_array[i] && !color.nil? }
             return true unless test_group.size < 4
